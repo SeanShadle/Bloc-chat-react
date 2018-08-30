953e15c193a04210e30f0b58e633d4bc038724e9
Sean Shadle <SeanDShadle@gmail.com>
2018-08-29 21:47:50 -0500

    Updated key in checkpoint 2

--- src/components/RoomList.js
+++ src/components/RoomList.js
@@ -1,39 +1,39 @@
 import React, { Component } from 'react';
 import * as firebase from 'firebase';
- class RoomList extends Component {
+class RoomList extends Component {
 
-    constructor(props){
-   super(props);
-   this.state = {
-     rooms: []
-   };
-   this.roomsRef = firebase.database().ref('rooms');
- }
-
- componentDidMount() {
-     this.roomsRef.on('child_added', snapshot => {
-     const room = snapshot.val();
-     room.key = snapshot.key;
-     console.log(room.name,room.key);
-     this.setState({ rooms: this.state.rooms.concat( room ) });
-   });
+  constructor(props) {
+    super(props);
+    this.state = {
+      rooms: []
+    };
+    this.roomsRef = firebase.database().ref('rooms');
   }
 
-formatName(str) {
-  var len = str.length;
-  var root = str.slice(0, len-1);
-  var end = str.slice(-1);
-  return root.charAt(0).toUpperCase() + root.slice(1) + " " + end;
-}
-   render(){
-   return(
-     <div>
-       {
-        this.state.rooms.map ( ( room, key ) =>
-         <p>{this.formatName(room.name)}</p>
-        )}
+  componentDidMount() {
+    this.roomsRef.on('child_added', snapshot => {
+      const room = snapshot.val();
+      room.key = snapshot.key;
+      console.log(room.name, room.key);
+      this.setState({ rooms: this.state.rooms.concat(room) });
+    });
+  }
+
+  formatName(str) {
+    var len = str.length;
+    var root = str.slice(0, len - 1);
+    var end = str.slice(-1);
+    return root.charAt(0).toUpperCase() + root.slice(1) + " " + end;
+  }
+  render() {
+    return (
+      <div>
+        {
+          this.state.rooms.map((room, key) =>
+            <p key={key}>{this.formatName(room.name)}</p>
+          )}
       </div>
-   );
+    );
   }
 }
- export default RoomList;
+export default RoomList;
